9b683d02b064e57a2a9ab0e6659f7af59c352b6d
Brandon <jhonnbrxndon@gmail.com>
2022-05-06 05:02:02 -0400

    Add Icon + Configuration Permissions

--- lib/global/environment.dart
+++ lib/global/environment.dart
@@ -3,6 +3,6 @@
 import 'dart:io';
 
 class Environment {
-  static String apiUrl = Platform.isAndroid ? 'http://10.0.2.2:3000/api' : 'http://localhost:3000/api';
-  static String socketUrl = Platform.isAndroid ? 'http://10.0.2.2:3000' : 'http://localhost:3000';
+  static String apiUrl = Platform.isAndroid ? 'https://app-chat-srv.herokuapp.com/api' : 'https://app-chat-srv.herokuapp.com/api';
+  static String socketUrl = Platform.isAndroid ? 'https://app-chat-srv.herokuapp.com/' : 'https://app-chat-srv.herokuapp.com/';
 }
